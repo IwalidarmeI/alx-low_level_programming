69ac659e624b5c5621a1f111590262aa9ce573fc
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-13 14:16:43 -0700

    Adding task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this main function
+ * description - this program calculate size of data types
+ * Return: returns 0
+ */
+int main(void)
+{
+	int integer;
+	long int longInt;
+	long long int longlongInt;
+	char character;
+	float floatT;
+
+	printf("Size of a char:%2ld\n", sizeof(character));
+	printf("Size of an int:%2ld\n", sizeof(integer));
+	printf("Size of a long int:%2ld\n", sizeof(longInt));
+	printf("Size of a long long int:%2ld\n", sizeof(longlongInt));
+	printf("Size of a float:%2ld\n", sizeof(floatT));
+	return (0);
+}
