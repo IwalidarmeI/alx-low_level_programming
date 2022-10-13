4bf2e0c9940a820c3949babbccdc4ef72b79cbe7
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-13 13:05:15 -0700

    Adding task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - this is the main function
+ * description -  this the begening of the program
+ * return - the return value is zero
+ */
+int main(void)
 {
-     char Mystr [] = "\"Programming is like building a multilingual puzzle";
-     puts(Mystr);
-     return(0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
 }
