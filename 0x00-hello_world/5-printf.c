86c728a85a9679931532f717d925afa429843545
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-13 13:39:33 -0700

    Adding task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * descriptio - begining of program
+ *Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
