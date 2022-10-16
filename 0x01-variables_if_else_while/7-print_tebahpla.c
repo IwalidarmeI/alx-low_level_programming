c474922ea94be80b5a72ff52c5c97bc9c4b5e75b
WALID BOUGHRARA <wd.boughrara@gmail.com>
2022-10-16 11:19:51 +0000

    Adding task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * description - this program display alphabet in a lowercase
+ * Return: returns value 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
