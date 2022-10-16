3cab35755fb9b74a2539dcc7cd885fa3055a3a7a
WALID BOUGHRARA <wd.boughrara@gmail.com>
2022-10-16 11:13:06 +0000

    Adding task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * description - this program display alphabet in a lowercase
+ * Return: returns value 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("%d", n);
+	}
+
+	return (0);
+}
