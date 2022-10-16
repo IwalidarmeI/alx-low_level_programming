401dacda9eab5b9e8dbbde8a935f34421a1d44e1
WALID BOUGHRARA <wd.boughrara@gmail.com>
2022-10-16 09:55:47 +0000

    Adding task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * description - this program display alphabet in a lowercase
+ * Return: returns value 0
+ */
+int main (void) 
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
