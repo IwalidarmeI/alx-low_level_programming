9c752f753d0f62d463ec3d3b2b55afaf0724911a
WALID BOUGHRARA <wd.boughrara@gmail.com>
2022-10-17 10:38:29 +0000

    Adding task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * description - this program display alphabet in a lowercase
+ * Return: returns value 0
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		for (ch = '0' ; ch <= '9' ; ch++)
+		{
+			if (n >= ch)
+				continue;
+
+			putchar(n);
+			putchar(ch);
+
+			if (n == '8' && ch == '9')
+				continue;
+			
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
