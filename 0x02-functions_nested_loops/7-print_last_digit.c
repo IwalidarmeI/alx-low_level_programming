35a69da9840b62ca205d3a365720072856421806
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 15:00:18 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - this finction print alphabet in lowercase
+ *
+ * @num: character input
+ * Return: this will not return any value
+ */
+int print_last_digit(int num)
+{
+	int lastDigit = num % 10;
+	return (lastDigit);
+}
