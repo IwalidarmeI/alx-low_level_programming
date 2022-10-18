c274b49feb634edd0bec0e513ce5e9a6d9b748b8
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 14:06:39 -0700

    Adding task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: character input
  * Return: this will not return any value
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
 		return (1);
