44024d27d148ad857897f0bdddeba3f0f3737a44
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 14:50:38 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - this finction print alphabet in lowercase
+ *
+ * @int: character input
+ * Return: this will not return any value
+ */
+int _abs(int)
+{
+	if (int < 0) 
+		return (int * int);
+	else
+		return (int);
+}
