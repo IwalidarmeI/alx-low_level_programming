bd96e86627c79086046dca9de30f97a77613f212
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 09:43:34 -0700

    Adding task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,3 @@
  *
  * Return: this will not return any value
  */
-void print_alphabet(void)
-{
-	int n;
-
-	for (n = 'a' ; n <= 'z' ; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
-}
