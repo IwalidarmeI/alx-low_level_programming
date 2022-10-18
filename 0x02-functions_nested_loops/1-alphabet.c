2a6a42ecad70c45678dd727993e096f3589ce91c
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 01:26:38 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - this finction print alphabet in lowercase
+ *
+ * Return: this will not return any value
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return;
+}
