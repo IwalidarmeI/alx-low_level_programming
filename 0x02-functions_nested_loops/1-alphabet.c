7b0ba7701933f2fde46592ea3ee8041a43450f20
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-18 12:40:18 -0700

    A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include "main.h"
 /**
  * print_alphabet - this finction print alphabet in lowercase
  *
  * Return: this will not return any value
  */
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(n);
+	}
+	return;
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.h
@@ -0,0 +1,6 @@
+#ifndef 1-ALPHABET_H_
+#define 1-ALPHABET_H_
+
+void print_alphabet_x10(void);
+
+#endif
