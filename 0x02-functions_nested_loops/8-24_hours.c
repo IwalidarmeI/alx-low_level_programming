601f06276be7a6c6ececb67ddfc666838e364f84
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-19 09:19:25 -0700

    Adding task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,14 +7,14 @@
  */
 void jack_bauer(void)
 {
-	int a,b,c,d;
+	int a, b, c, d;
 
 	for (a = '0' ; a <= '2' ; a++)
 	{
 		for (b = '0' ; b <= '9' ; b++)
 		{
 			if (a == '2' && b == '4')
-		       		break;	
+				break;
 			for (c = '0' ; c <= '5' ; c++)
 			{
 				for (d = '0' ; d <= '9' ; d++)
