d63b20061b490be7db037140adf0231d1834aa2c
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-19 13:37:19 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - this finction do the addition
+ *
+ * @a: first number input
+ * @b: second number input
+ * Return: this will not return any value
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 #include <stdio.h>
+int add(int, int);
