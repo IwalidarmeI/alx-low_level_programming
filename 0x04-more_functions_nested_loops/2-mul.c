939366f44c8a304f50330aa7e0fd034869ad68c6
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-20 02:40:30 -0700

    Adding task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - this finction do the multuplicaition
+ *
+ * @a: first number input
+ * @b: second number input
+ * Return: this will not return any value
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
