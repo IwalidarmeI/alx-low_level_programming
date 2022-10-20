baca2bdc725a963b199d0345d0cac37c26d0dd87
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-20 00:44:32 -0700

    Adding task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - this fonction checks for uppercase characters
+ *
+ * @c: the input of the function
+ * Return : returns 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the 4th project of learning c
