101145c6ccbe4d30064a75350e5d13f9cd10a44b
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-20 03:38:20 -0700

    Adding task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - this fonction checks for linee
+ *
+ * @n : the number of the underscore
+ * Return : no returns for this function
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int sp;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1 ; i <= n ; i++)
+		{
+			for (sp = 0 ; sp <= i - 1 ; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n';
+		}
+	}
+}
