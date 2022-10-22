5ef8bf4022b7b3a3d454e97506a4eea0fdb45c6d
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-22 01:53:10 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - this function print to 98
+ *
+ * @n: the number from the input
+ * Return: Returns nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+	int c;
+
+	if (n <= 98)
+	{
+		c = i++
+	}
+	else
+	{
+		c = i--
+	}
+	for (i = n ; i <= 98 ; c)
+	{
+		if (i == 98)
+		{
+			printf("%d", i);
+		}
+		printf("%d, ", i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 #include <stdio.h>
 int add(int, int);
+void print_to_98(int n);
