beaaf801a9f82c2c97d481c45b31d5fc4224c33e
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-22 03:07:40 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the start of the program
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int f1 = 1;
+	int f2 = 2;
+
+	for (i = 0 ; i <= 50 ; i++)
+	{
+		f1 = f1 + f2;
+	}
+}	
