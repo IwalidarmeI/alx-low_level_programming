b1f8fee4eae1f1e71104e5a06fcef39054fe2f0f
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-22 02:55:17 -0700

    Adding task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the start of the program
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+		else
+			continue;
+
+		printf("%d\n", sum);
+	}
+}
