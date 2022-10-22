26983c3922a4f1f860d6f98e5d67ba0dccaaf720
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-22 03:45:49 -0700

    Adding task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,13 +10,18 @@ int main(void)
 	long int f1 = 1;
 	long int f2 = 1;
 	int i;
+	long int b;
 
 	printf("1, ");
 	for (i = 2 ; i <= 98 ; i++)
 	{
 		f1 = f1 + f2;
 		f2 = f1 - f2;
-		if (i == 98)
+		if (i > 89)
+			f1 = f1 / 1000;
+			b = f1 % 1000;
+			printf("%ld%ld",f1 , b);
+		else if (i == 98)
 		{
 			printf("%ld\n", f1);
 		}
