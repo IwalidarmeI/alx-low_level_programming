b2568b3b74648b77866fbb785543d8aa6025ac89
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-22 03:26:17 -0700

    Adding task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	int f1 = 1;
-	int f2 = 2;
+	int f1 = 0;
+	int f2 = 1;
 	int i;
 
 	for (i = 0 ; i <= 50 ; i++)
 	{
 		f1 = f1 + f2;
+		f2 = f1 - f2;
 		printf("%d, ", f1);
 	}
 	return (0);
