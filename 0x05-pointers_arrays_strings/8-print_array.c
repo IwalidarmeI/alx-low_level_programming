1abd82e29297727dcae735f0fd350ebb791dea90
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-24 13:51:38 -0700

    Adding task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,8 @@ void print_array(int *a, int n)
 	
 	for (i = 0 ; i < n ; i++)
 	{	if (i == n - 1)
-			printf("%ls\n", a);
+			printf("%d\n", a[i]);
 		else
-			printf("%ls, ", a);
+			printf("%d, ", a[i]);
 	}
 }
