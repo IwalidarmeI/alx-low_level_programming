fc51258e7350cfb58aea32fadfa4cc5c7e7d78e8
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-24 11:26:44 -0700

    Adding task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - this function to print a string
+ *
+ * @s: this is the string
+ * Return: Returns nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
