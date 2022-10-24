b2c34fff5a8eeff42ab17059512d09e58c335130
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-24 11:16:00 -0700

    Adding task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - this function to print a string
+ *
+ * @str: this is the string
+ * Return: Returns nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
