29d8a29a3d69fac815b2589518a96c4ce25b1f18
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-10-25 09:20:16 -0700

    Adding task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy to the buffer
+ *
+ * @dest: the destination
+ * @src: the source
+ * Return: Returns the value of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src [i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
