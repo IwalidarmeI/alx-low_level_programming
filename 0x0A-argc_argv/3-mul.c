ef3d1c0785b46d886e80a0493455c25c251b939a
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-11-04 03:05:38 -0700

    Adding project 0x0A

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this the begin of the program
+ * @argc: this the counter of the arguments
+ * @argv: this the array
+ * Return: Reurns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 0 ; i < argc ; i++)
+		{
+			mul = atoi(argv[i]) * mul;
+		}
+		printf("%d\n", mul);
+		return(0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
