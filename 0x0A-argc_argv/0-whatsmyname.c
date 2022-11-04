5cdbd362c23e6e4002be0a263aa81f97beddd8f8
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-11-04 02:28:21 -0700

    Adding project 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this the begin of the program
+ * @argc: this the counter of the arguments
+ * @argv: this the array
+ * Return: Reurns 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
