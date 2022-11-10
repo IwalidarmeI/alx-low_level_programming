d1f2ec99313c1cf92cd92fa2b40bc3fc1fad2d69
Walid BOUGHRARA <wd.boughrara@gmail.com>
2022-11-10 11:30:04 -0800

    Adding task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_nconcat - function concetates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: numbers of bytes
+ * Return: returns a pointers
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,4 @@
 void *malloc_checked(unsigned int b);
 #include <stdlib.h>
 void *malloc(size_t size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
